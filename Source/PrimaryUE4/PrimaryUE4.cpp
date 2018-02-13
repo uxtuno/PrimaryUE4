b0ed41c9a255a95e5e337553b022d887ddccc536
DESKTOP-NVJO8FT\uxtuno <uxtuno664@gmail.com>
2018-02-14 01:24:37 +0900

    初回コミット

---
+++ Source/PrimaryUE4/PrimaryUE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PrimaryUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PrimaryUE4, "PrimaryUE4" );
---
+++ Source/PrimaryUE4/PrimaryUE4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PrimaryUE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRIMARYUE4_API APrimaryUE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
